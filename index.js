4da9f4ae0c4bbc7436728d4f8c91d0e03707def2
tinamanga <christinamanga28@gmail.com>
2025-03-28 01:47:44 +0300

    writing methods that use instances data and parameter data

--- index.js
+++ index.js
@@ -1,1 +1,57 @@
 // Your code here
+// Cat class
+class Cat {
+    constructor(name, sex) {
+      this.name = name; // instance property for the name
+      this.sex = sex;   // instance property for the sex
+    }
+  
+    // speak method for Cat
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+  
+  // Dog class
+  class Dog {
+    constructor(name, sex) {
+      this.name = name; // instance property for the name
+      this.sex = sex;   // instance property for the sex
+    }
+  
+    // speak method for Dog
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  
+  // Bird class
+  class Bird {
+    constructor(name, sex) {
+      this.name = name; // instance property for the name
+      this.sex = sex;   // instance property for the sex
+    }
+  
+    // speak method for Bird
+    speak() {
+      if (this.sex === 'male') {
+        return `It's me! ${this.name}, the parrot!`;
+      } else {
+        return `${this.name} says squawk!`;
+      }
+    }
+  }
+  
+  // Example Usage:
+  const cat = new Cat("Whiskers", "female");
+  console.log(cat.speak()); // Output: Whiskers says meow!
+  
+  const dog = new Dog("Buddy", "male");
+  console.log(dog.speak()); // Output: Buddy says woof!
+  
+  const birdMale = new Bird("Polly", "male");
+  console.log(birdMale.speak()); // Output: It's me! Polly, the parrot!
+  
+  const birdFemale = new Bird("Sky", "female");
+  console.log(birdFemale.speak()); // Output: Sky says squawk!
+  
